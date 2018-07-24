44559a1fea60aa2c4711036aba65fcd6e0a03728
dahoony <crysis1@naver.com>
2018-07-24 18:17:20 +0900

    delete update

--- src/com/st/frame/UserMoim.java
+++ src/com/st/frame/UserMoim.java
@@ -5,4 +5,5 @@ import java.util.Map;
 public interface UserMoim {
 	public void delete(String moimId) throws Exception;
 	public void deleteUser(Map<String, String> obj) throws Exception;
+	public void deleteMoim(Map<String, String> obj) throws Exception;
 }
